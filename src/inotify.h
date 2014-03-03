7b8265a6d5a4f152166ab8a9d5402622913d9228
George <somu@so.mu>
2014-03-04 02:08:06 +0400

    Move inotify-related code to inotify.h

---
+++ src/inotify.h
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/inotify.h>
+
+#define EVENT_SIZE  sizeof(struct inotify_event)
+
+struct watcher {
+  int fd;
+  int wd;
+};
+
+struct watcher watch(const char *file) {
+  int fd = inotify_init();
+  int wd = inotify_add_watch(fd, file, IN_MODIFY);
+  struct watcher watcher = {fd, wd};
+  return watcher;
+}
+
+inline void unwatch(struct watcher *watcher) {
+  (void) inotify_rm_watch(watcher->fd, watcher->wd);
+  (void) close(watcher->fd);
+}
--- src/lastlog.h
+++ src/lastlog.h
@@ -1,17 +1,1 @@
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/inotify.h>
-
-#define EVENT_SIZE  sizeof(struct inotify_event)
-#define BUF_LEN     1024 * (EVENT_SIZE + 16)
-
-int watch_lastlog() {
-  int fd = inotify_init();
-  int wd = inotify_add_watch(fd, "/var/log/lastlog", IN_MODIFY);
-}
-
-
-inline void close_lastlog(int fd, int wd) {
-  (void) inotify_rm_watch(fd, wd);
-  (void) close(fd);
-}
+#define LASTLOG "/var/log/lastlog"
