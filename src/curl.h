e042aec810bd5e1d2dbba24d56a68d1a09abe66d
George <somu@so.mu>
2014-03-04 02:08:30 +0400

    Write post params to char *post

--- src/curl.h
+++ src/curl.h
@@ -1,22 +1,20 @@
 #include <curl/curl.h>
 
-inline void curl_push(CURL *curl) {
-  curl_easy_perform(curl);
-}
-
 CURL *curl_init(const char *token, const char *user) {
   CURL *curl;
+  char *post;
  
   curl_global_init(CURL_GLOBAL_ALL);
   curl = curl_easy_init();
 
+  asprintf(&post, "token=%s&user=%s&message=%s", token, user, "Someone+has+logged+in!");
   curl_easy_setopt(curl, CURLOPT_URL, "https://api.pushover.net/1/messages.json");
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "token=&user=&message=");
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post);
 
   return curl;
 }
 
 inline void curl_cleanup(CURL *curl) {
-  curl_easy_cleanup(curl);
+  curl_easy_cleanup(&curl);
   curl_global_cleanup();
 }
