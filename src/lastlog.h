895daffc6866acb95957474c2e6423d50f135388
George <somu@so.mu>
2014-03-04 04:25:11 +0400

    LASTLOG → LL_PATH

--- src/lastlog.h
+++ src/lastlog.h
@@ -5,7 +5,7 @@
 #define LL_SSIZE sizeof(struct lastlog)
 
 signed int ll_count() {
-  FILE *fp = fopen(LASTLOG, "rb");
+  FILE *fp = fopen(LL_PATH, "rb");
   fseek(fp, 0, SEEK_END);
   signed int size = ftell(fp) / sizeof(struct lastlog);
   fclose(fp);
