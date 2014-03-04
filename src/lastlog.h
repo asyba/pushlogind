e236b054f8164899898fdc4a1d83d20aa7b38b8e
George <somu@so.mu>
2014-03-04 04:13:04 +0400

    Switch some names in sake of more readable code

--- src/lastlog.h
+++ src/lastlog.h
@@ -1,13 +1,13 @@
 #include <lastlog.h>
 #include <pwd.h>
 
-#define LASTLOG "/var/log/lastlog"
-#define LSIZE   sizeof(struct lastlog)
+#define LL_PATH  "/var/log/lastlog"
+#define LL_SSIZE sizeof(struct lastlog)
 
-signed int ll_size() {
+signed int ll_count() {
   FILE *fp = fopen(LASTLOG, "rb");
   fseek(fp, 0, SEEK_END);
-  signed int ssize = ftell(fp) / sizeof(struct lastlog);
+  signed int size = ftell(fp) / sizeof(struct lastlog);
   fclose(fp);
-  return ssize;
+  return size;
 }
--- src/pushlogind.c
+++ src/pushlogind.c
@@ -7,23 +7,31 @@
 
 int main() {
   CURL *curl = curl_init(TOKEN, USER);
-  struct watcher watcher = watch(LASTLOG);
+
+  /* Inotify-related: initialize a watcher,
+   * and create a 1-slot event buffer.
+   */
+  struct watcher watcher = watch(LL_PATH);
   char event[EVENT_SIZE];
-  ssize_t bytes;
-  signed int ssize = ll_size();
-  struct lastlog ll[ssize];
+
+  /* All about /var/log/lastlog: count
+   * number of possible users and allocate
+   * a buffer for parsing lastlog.
+   */
+  signed int users = ll_count();
+  struct lastlog ll[users];
   struct lastlog recent;
   signed int recent_id, i;
   FILE *fp;
 
   for(;;) {
     sleep(FREQUENCY);
-    bytes = read(watcher.fd, event, EVENT_SIZE);
-    if(bytes == EVENT_SIZE) {
-      fp = fopen(LASTLOG, "rb");
-      fread(ll, LSIZE, ssize, fp);
+
+    if(read(watcher.fd, event, EVENT_SIZE) == EVENT_SIZE) {
+      fp = fopen(LL_PATH, "rb");
+      fread(ll, LL_SSIZE, users, fp);
       
-      for(i = 0; i < ssize; i++) {
+      for(i = 0; i < users; i++) {
         if(ll[i].ll_time > recent.ll_time) {
           recent = ll[i];
           recent_id = i;
