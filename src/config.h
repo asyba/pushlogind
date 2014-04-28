d0c132b2f93191620b4871c94c06dd28552fb160
George <somu@so.mu>
2014-04-29 03:23:20 +0400

    to get it → for how to get it

--- src/config.h
+++ src/config.h
@@ -1,4 +1,4 @@
 #define USER "none"  /* Your user token. Get it at https://pushover.net */
-#define TOKEN "none" /* Your application token. See README to get it */
+#define TOKEN "none" /* Your application token. See README for how to get it */
 #define PRIORITY "1" /* https://pushover.net/api#priority */
 #define SOUND "bike" /* https://pushover.net/api#sounds */
