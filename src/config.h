a80196684b4b494432d64367391f8d8999e72253
George <somu@so.mu>
2014-04-29 03:22:00 +0400

    Default is bike, not persistent

--- src/config.h
+++ src/config.h
@@ -1,4 +1,4 @@
-#define USER "none"        /* Your user token. Get it at https://pushover.net */
-#define TOKEN "none"       /* Your application token. See README to get it */
-#define PRIORITY "1"       /* https://pushover.net/api#priority */
-#define SOUND "persistent" /* https://pushover.net/api#sounds */
+#define USER "none"  /* Your user token. Get it at https://pushover.net */
+#define TOKEN "none" /* Your application token. See README to get it */
+#define PRIORITY "1" /* https://pushover.net/api#priority */
+#define SOUND "bike" /* https://pushover.net/api#sounds */
