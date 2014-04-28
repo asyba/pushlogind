b4a7236d52e85e44dfbc89c6cc135008c0df61bd
George <somu@so.mu>
2014-04-29 03:08:18 +0400

    Rename curl.c to pushover.c

--- src/pushover.c
+++ src/pushover.c
@@ -4,7 +4,11 @@
 #define QUERY "token=" TOKEN "&user=" USER "&sound=" SOUND \
               "&priority=" PRIORITY "&message=%s+has+logged+in+from+%s+(%s)"
 
-CURL *curl_init() {
+/**
+ * Returns a CURL struct that is pointing
+ * to Pushover API endpoint.
+ */
+CURL *push_init() {
   CURL *curl;
   curl_global_init(CURL_GLOBAL_ALL);
   curl = curl_easy_init();
@@ -13,19 +17,26 @@ CURL *curl_init() {
   return curl;
 }
 
-/* Pushes a login message using information from
- * ll_user struct (see lastlog.h) and config.h:
+/**
+ * Pushes a login message to Pushover using information
+ * from ll_user struct (see lastlog.c and QUERY):
  */
-static inline void curl_push(CURL *curl, struct ll_user user) {
+static inline void push(CURL *curl, struct ll_user user) {
   char *post;
-  asprintf(&post, QUERY, getpwuid(user.id)->pw_name, user.ll.ll_host, user.ll.ll_line);
+  asprintf(&post, QUERY,
+    getpwuid(user.id)->pw_name, user.ll.ll_host, user.ll.ll_line);
 
   curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post);
   curl_easy_perform(curl);
+
   free(post);
 }
 
-inline void curl_cleanup(CURL *curl) {
+/**
+ * Frees the space allocated for both CURL struct
+ * and all other cURL entities.
+ */
+inline void push_cleanup(CURL *curl) {
   curl_easy_cleanup(&curl);
   curl_global_cleanup();
 }
