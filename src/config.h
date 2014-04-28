b2bb01c691e7b391acccdfc1887b08a855c8fbff
George <somu@so.mu>
2014-04-29 03:08:07 +0400

    Document config.h

--- src/config.h
+++ src/config.h
@@ -1,4 +1,4 @@
-#define USER None
-#define TOKEN None
-#define PRIORITY "0"
-#define SOUND "bike"
+#define USER "none"        /* Your user token. Get it at https://pushover.net */
+#define TOKEN "none"       /* Your application token. See README to get it */
+#define PRIORITY "1"       /* https://pushover.net/api#priority */
+#define SOUND "persistent" /* https://pushover.net/api#sounds */
